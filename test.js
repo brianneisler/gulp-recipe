cf6b7bf39b01d613628aa29a490352fa0d204794
Brian Neisler <brian@airbug.com>
2015-12-21 00:21:28 -0800

    First working pass of gulp-elf

--- test.js
+++
@@ -1,7 +0,0 @@
-var npm = require('npm');
-
-npm.load(function(err, npm) {
-    npm.commands.ls([], true, function(err, data, lite) {
-        console.log(data); //or lite for simplified output
-    });
-});
