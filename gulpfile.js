371bef52546a157a3d41066b4bd15ea07dd68aa0
Brian Neisler <brian@airbug.com>
2015-12-21 22:16:37 -0800

    Few more fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp            = require('gulp');
 var recipe          = require('gulp-recipe');
 
-gulp.task('lint', recipe.getRecipe('eslint', [
+gulp.task('lint', recipe.get('eslint', [
     'lib/**/*.js',
     'scripts/**/*.js',
     '!node_modules/**'
